e4f68de7d185cf95164538670b29dce7b890b96e
jonslucas <jon.s.lucas@gmail.com>
2015-07-30 23:40:25 -0400

    Initial Commit

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/nightlife-dev'
+  },
+
+  seedDB: true
+};

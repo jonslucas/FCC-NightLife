2dfea12a39362bb27a1d52e47e4ef275160e9771
jonslucas <jon.s.lucas@gmail.com>
2015-08-02 23:29:04 -0400

    Began building out yelp integration.

--- client/app/Yelp/Yelp.service.js
+++ client/app/Yelp/Yelp.service.js
@@ -1,10 +1,13 @@
 'use strict';
 
 angular.module('nightLifeApp')
-  .factory('Yelp', function () {
+  .factory('Yelp', function ($http) {
     // Service logic
     // ...
-
+    navigator.geolocation.getCurrentPosition(function (position) {
+      console.log(position.coords.latitude);
+      console.log(position.coords.longitude);
+    });
     var meaningOfLife = 42;
 
     // Public API here

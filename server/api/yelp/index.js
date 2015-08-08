52a765aac742890dcdcd3e99cc8249dfe42c0662
jonslucas <jon.s.lucas@gmail.com>
2015-08-07 22:45:15 -0400

    Continued testing for node-yelp integration.

--- server/api/yelp/index.js
+++ server/api/yelp/index.js
@@ -6,8 +6,8 @@ var controller = require('./yelp.controller');
 
 var router = express.Router();
 
-router.get('/', controller.index);
-router.get('/:id', controller.show);
+router.get('/', controller.search);
+router.get('/:id', controller.search);
 router.post('/', controller.create);
 router.put('/:id', controller.update);
 router.patch('/:id', controller.update);
--- server/api/yelp/yelp.controller.js
+++ server/api/yelp/yelp.controller.js
@@ -3,16 +3,25 @@
 var _ = require('lodash');
 var Yelp = require('./yelp.model');
 var yConfig = require('../../config/environment').yelp;
+//console.log('ycConfig: '+JSON.stringify(yConfig));
 var yelp = require('yelp').createClient(yConfig);
+
 //console.log('yelp: '+JSON.stringify(yelp));
-//var search = function(req, res) {
-//  console.log('req.params.zip'+req.params.zip);
-//  yelp.search({term: 'bar', location: '28739'}, function(err, bars){
-//    if(err) { return handleError(res, err); }
-//    if(!bars) { return res.send(404); }
-//    return res.json(bars);
-//  });
-//};
+yelp.search({term: 'bar', location: '28739'}, function(err, bars){
+  if(err) { return console.log(err); }
+  if(!bars) { return console.log(404); }
+  //return res.json(bars);
+  return console.log('bars: '+JSON.stringify(bars));
+});
+
+exports.search = function(req, res) {
+  console.log('req.params.zip: '+req.params.zip);
+  yelp.search({term: 'bar', location: '28739'}, function(err, bars){
+    if(err) { return handleError(res, err); }
+    if(!bars) { return res.send(404); }
+    return res.json(bars);
+  });
+};
 
 
 // Get list of yelps

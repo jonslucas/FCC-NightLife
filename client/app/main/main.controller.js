194ab3f83dbb76bac7ac47a09476bdb963343173
jonslucas <jon.s.lucas@gmail.com>
2015-08-08 23:47:11 -0400

    Removed the yelp endpoint model and references to model. Removed log from main ctrl.

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -4,7 +4,6 @@ angular.module('nightLifeApp')
   .controller('MainCtrl', function ($scope, Yelp) {
     Yelp.search('28739', function (err, resp) {
       if(err) { return console.error(err); }
-      console.log(JSON.stringify(resp));
       $scope.bars = resp.businesses;
     });
     $scope.message = 'Hello';
--- server/api/yelp/yelp.controller.js
+++ server/api/yelp/yelp.controller.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var _ = require('lodash');
-var Yelp = require('./yelp.model');
 var yConfig = require('../../config/environment').yelp;
 var yelp = require('yelp').createClient(yConfig);
 
@@ -19,53 +18,27 @@ exports.search = function(req, res) {
 
 // Get list of yelps
 exports.index = function(req, res) {
-  Yelp.find(function (err, yelps) {
-    if(err) { return handleError(res, err); }
-    return res.json(200, yelps);
-  });
+
 };
 
 // Get a single yelp
 exports.show = function(req, res) {
-  Yelp.findById(req.params.id, function (err, yelp) {
-    if(err) { return handleError(res, err); }
-    if(!yelp) { return res.send(404); }
-    return res.json(yelp);
-  });
+
 };
 
 // Creates a new yelp in the DB.
 exports.create = function(req, res) {
-  Yelp.create(req.body, function(err, yelp) {
-    if(err) { return handleError(res, err); }
-    return res.json(201, yelp);
-  });
+
 };
 
 // Updates an existing yelp in the DB.
 exports.update = function(req, res) {
-  if(req.body._id) { delete req.body._id; }
-  Yelp.findById(req.params.id, function (err, yelp) {
-    if (err) { return handleError(res, err); }
-    if(!yelp) { return res.send(404); }
-    var updated = _.merge(yelp, req.body);
-    updated.save(function (err) {
-      if (err) { return handleError(res, err); }
-      return res.json(200, yelp);
-    });
-  });
+
 };
 
 // Deletes a yelp from the DB.
 exports.destroy = function(req, res) {
-  Yelp.findById(req.params.id, function (err, yelp) {
-    if(err) { return handleError(res, err); }
-    if(!yelp) { return res.send(404); }
-    yelp.remove(function(err) {
-      if(err) { return handleError(res, err); }
-      return res.send(204);
-    });
-  });
+
 };
 
 function handleError(res, err) {

0b91cb68cab54a66842e08238bcfd622e7060ab1
jonslucas <jon.s.lucas@gmail.com>
2015-08-16 23:55:49 -0400

    Working on staggering the data in bars.

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -3,11 +3,28 @@
 angular.module('nightLifeApp')
   .controller('MainCtrl', function ($scope, $timeout, Yelp) {
     $scope.bars = [];
+    var yelpData = null;
     $scope.search= function () {
       if(!$scope.location) { return; }
       Yelp.search($scope.location, function (err, resp) {
         if(err) { return console.error(err); }
-        $scope.bars = resp.businesses;
+        yelpData = resp.businesses;
+        //for(var i =0; i<yelpData.length; i++) {
+        //  console.log('i: '+i);
+        //  $timeout(function () {
+        //    $scope.bars.push(yelpData.shift());
+        //
+        //  }, 1000);
+        //}
+
       });
+
     };
+    $scope.addToBars = function () {
+      if(yelpData.length>0) {
+        $scope.bars.push(yelpData.shift());
+      }
+
+    };
+
   });

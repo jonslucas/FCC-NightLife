20bb34fe5f64c4d5f02ba932fbab486164cafcf6
jonslucas <jon.s.lucas@gmail.com>
2015-08-17 21:54:59 -0400

    Began playing with GSAP animations.

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -4,22 +4,29 @@ angular.module('nightLifeApp')
   .controller('MainCtrl', function ($scope, $timeout, Yelp) {
     $scope.bars = [];
     var yelpData = null;
+    var delayAddTo = function(bar) {
+      //console.log('Inside delayAddTo');
+      var b = bar;
+      $scope.bars.push(b);
+    };
     $scope.search= function () {
       if(!$scope.location) { return; }
       Yelp.search($scope.location, function (err, resp) {
         if(err) { return console.error(err); }
         yelpData = resp.businesses;
-        //for(var i =0; i<yelpData.length; i++) {
-        //  console.log('i: '+i);
-        //  $timeout(function () {
-        //    $scope.bars.push(yelpData.shift());
-        //
-        //  }, 1000);
-        //}
+        var bar = yelpData.shift(),
+          start = yelpData.length,
+              i = 0;
+        delayAddTo(bar);
+        for(i; i<start; i++) {
+          //console.log('i: '+i);
+          $timeout(delayAddTo(yelpData.shift()), 5000);
+        }
 
       });
 
     };
+
     $scope.addToBars = function () {
       if(yelpData.length>0) {
         $scope.bars.push(yelpData.shift());

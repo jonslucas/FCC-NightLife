fabf6c79625a05418f161ae61e043c3187a3d30c
jonslucas <jon.s.lucas@gmail.com>
2015-08-19 14:39:35 -0400

    Cleaned up unused func from Yelp factory.  Added missing semicolon.

--- client/app/Yelp/Yelp.service.js
+++ client/app/Yelp/Yelp.service.js
@@ -3,26 +3,6 @@
 angular.module('nightLifeApp')
   .factory('Yelp', function ($http) {
 
-    //example url:
-    //http://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452
-
-
-    var get_coords = function() {
-      var lat,
-          long;
-      navigator.geolocation.getCurrentPosition(function (position) {
-        console.log(position.coords.latitude);
-        console.log(position.coords.longitude);
-        lat = position.coords.latitude;
-        long = position.coords.longitude;
-      });
-      return {
-        lat:lat,
-        lon: long
-      };
-    };
-
-    // Public API here
     return {
       search: function(location, cb) {
         $http({
@@ -35,7 +15,7 @@ angular.module('nightLifeApp')
           cb(null, data);
         }).error(function (err) {
           cb(err);
-        })
+        });
       }
     };
   });

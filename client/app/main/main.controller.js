cadb4ddf207903c69e05f61ecb9a964881b17557
jonslucas <jon.s.lucas@gmail.com>
2015-08-22 02:22:23 -0400

    Added auth intercept to check-in button.  Bonus still missing.

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,9 +1,12 @@
 'use strict';
 
 angular.module('nightLifeApp')
-  .controller('MainCtrl', function ($scope, $timeout, $document, Yelp) {
+  .controller('MainCtrl', function ($scope, Yelp) {
+    console.log('search: '+Yelp.getData());
     $scope.search= function () {
       if(!$scope.location) { return; }
+      Yelp.storeData($scope.location);
+      console.log('inner_search: '+Yelp.getData());
       Yelp.search($scope.location, function (err, resp) {
         if(err) { return console.error(err); }
         $scope.bars = resp.businesses;
--- client/app/yelpBar/yelpBar.directive.js
+++ client/app/yelpBar/yelpBar.directive.js
@@ -1,12 +1,15 @@
 'use strict';
 
-var ctrl = function($scope) {
+var ctrl = function($scope, $location, Auth) {
   var ctrl = this;
   ctrl.added = false;
   ctrl.going = 0;
   ctrl.goingMsg = 'Check-in';
   ctrl.star = ($scope.bar.rating>=3);
   ctrl.checkIn = function() {
+    if(!Auth.isLoggedIn()) {
+      $location.path('/login');
+    }
     if(ctrl.added===true) {
       ctrl.going -=1;
       ctrl.goingMsg = 'Check-in';
